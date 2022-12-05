ab8649660c911414bc14e51752407d9164da9c66
JSerGit <bibik.serej@gmail.com>
2022-12-05 23:08:21 +0200

    @types/node

---
+++ index.ts
@@ -0,0 +1,5 @@
+/// <reference types="node" />
+export * from './src/startProcessDeathWatching'
+export * from './src/watchProcessDeath'
+// process.kill(process.pid, 'SIGUSR2')
+// process.kill(process.ppid, 'SIGUSR2')
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './startProcessDeathWatching'
-export * from './watchProcessDeath'
-// process.kill(process.pid, 'SIGUSR2')
-// process.kill(process.ppid, 'SIGUSR2')
-console.log('12312311')
