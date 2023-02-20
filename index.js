cc382c94a92e18d208997eb31eb29ee4df01fb3d
JSerGit <bibik.serej@gmail.com>
2023-02-21 01:21:39 +0200

    add ts transpile layer

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const path = require('node:path')
 const { execFileSync } = require('node:child_process')
 const pathToTsc = path.join(__dirname, 'node_modules/typescript/bin/tsc')
 const pathToTsConfig = path.join(__dirname, 'tsconfig.json')
-const tsconfig = require(pathToTsConfig)
-const outDir = tsconfig.compilerOptions.outDir
 execFileSync(process.execPath, [pathToTsc, '--project', pathToTsConfig], {
     cwd: __dirname,
 })
-const transpiledModule = require(outDir)
+const transpiledModule = require('./lib')
 module.exports = transpiledModule
