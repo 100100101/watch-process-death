3369dd8f1a3ce31bdeeb3eef437431be10df3b85
JSerGit <bibik.serej@gmail.com>
2023-02-20 17:15:58 +0200

    add ts transpile layer

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 /// <reference types="node" />
-export * from './src/startProcessDeathWatching'
-export * from './src/watchProcessDeath'
+export * from './startProcessDeathWatching'
+export * from './watchProcessDeath'
 // process.kill(process.pid, 'SIGUSR2')
 // process.kill(process.ppid, 'SIGUSR2')
